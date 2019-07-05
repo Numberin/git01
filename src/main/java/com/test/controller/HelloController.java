baf9b74ecc4365b35bf284bb19298faeccd1963e
xyl-github <a1970382706@163.com>
2019-07-05 11:36:15 +0800

    tianjia

--- src/main/java/com/test/controller/HelloController.java
+++ src/main/java/com/test/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.test.controller;
 
 public class HelloController {
     public static void main(String[] args) {
+        System.out.println("hello world");
         System.out.println("hello");
     }
 }
