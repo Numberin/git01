c2877fa83ca9c9c423675b5f2a02b7a1e4235cf1
lol <353959541@qq.com>
2019-08-14 16:57:33 +0800

    1

--- src/main/java/com/test/controller/HelloController.java
+++ src/main/java/com/test/controller/HelloController.java
@@ -3,6 +3,6 @@ package com.test.controller;
 public class HelloController {
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("hello");
+//        System.out.println("hello");
     }
 }
